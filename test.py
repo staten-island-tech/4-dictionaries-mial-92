4d0d5a649e1d8cc5ab2b99f79d39626073958943
mial-92 <mialin0170@gmail.com>
2025-10-07 07:16:32 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-""" MiasDealerShop = [
+MiasDealerShop = [
     {"Item": "1. ella's poopy shoes", "price": 0.56, "description": "During a fire drill, third period, Ella step into some not so good, solid brown substance. It was unfortunate But thankfully she was able to wash it off! However, there may be some residue in the creases of her shoes."},                                                           
     {"Item": "2. olivia's cat's hairballs", "price": 4.99, "description": "Olivia is allergic to cats, however, she still has two very adorable cats, Ollie and Ellie! Her cats are kinda hairy and she just has a bunch of hair stuck around her home."},
     {"Item": "3. BigBackLiver's Gooner Fanfic(resale)", "price": 350.00, "description": "Collaboration with BigBack Liver's Library!! Buy now before it's too late..."},
@@ -10,24 +10,45 @@
 
 for i in range(0, len(MiasDealerShop)):
     print(MiasDealerShop[i]["Item"])
-Cart = input("What would you like to buy?") """
+Cart = input("What would you like to buy?")
+
+
+
+
 """ 
 def language(x): 
     x = x.lower()
     scount = x.count("s")
+    scount = x.count("S")
     tcount = x.count("t")
+    tcount = x.count("T")
     if scount > tcount:
         print("English")
     elif tcount >= scount:
         print("French")
     else:
         print("huh")
-language("stinky")
- """
+language("STinky")
+
+def language(x):
+    s = 0
+    t = 0
+    char = x
+    if char in x:
+        if char == "s" or char == "S":
+            s += 1
+        elif char == "t" or char == "T":
+            t += 1
+    if s > t:
+        print("English")
+    else: 
+        print("French")
+language("Stink")
+
 def parkingspace(x):
     ccount = x.count("C")
     CCcount = x.count("CC") 
     CCcount = CCcount * 2
     print(ccount-CCcount)
 
-parkingspace("CC..C.CC.C.CC.C.")
+parkingspace("CC..C.CC.C.CC.C.") """
