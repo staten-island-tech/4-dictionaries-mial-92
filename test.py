9f29ce4a859833292bac5d3a3eafcb3483ec5969
mial-92 <mialin0170@gmail.com>
2025-10-14 07:17:11 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-""" MiasDealerShop = [
+MiasDealerShop = [
     {"Item": "1. ella's poopy shoes", "price": 0.56, "description": "During a fire drill, third period, Ella step into some not so good, solid brown substance. It was unfortunate But thankfully she was able to wash it off! However, there may be some residue in the creases of her shoes."},                                                           
     {"Item": "2. olivia's cat's hairballs", "price": 4.99, "description": "Olivia is allergic to cats, however, she still has two very adorable cats, Ollie and Ellie! Her cats are kinda hairy and she just has a bunch of hair stuck around her home."},
     {"Item": "3. BigBackLiver's Gooner Fanfic(resale)", "price": 350.00, "description": "Collaboration with BigBack Liver's Library!! Buy now before it's too late..."},
@@ -17,7 +17,7 @@ consent = True
 while consent == True:
     for item in MiasDealerShop:
         if Cart in item["Item"]:
-            print(f"{item["Item"]} has been added to your cart")
+            print(f"{["Item"]} has been added to your cart")
     
     consent = input("Would you like to add more into your cart? ")
     if consent == "yes":
@@ -25,63 +25,3 @@ while consent == True:
         consent = True
     if consent == "no":
         print("Ok! Time to check out.") 
- """
-
-""" def language(x): 
-    x = x.lower()
-    scount = x.count("s")
-    scount = x.count("S")
-    tcount = x.count("t")
-    tcount = x.count("T")
-    if scount > tcount:
-        print("English")
-    elif tcount >= scount:
-        print("French")
-    else:
-        print("huh")
-language("STinky")
-
-def language(x):
-    s = 0
-    t = 0
-    char = x
-    if char in x:
-        if char == "s" or char == "S":
-            s += 1
-        elif char == "t" or char == "T":
-            t += 1
-    if s > t:
-        print("English")
-    else: 
-        print("French")
-language("Stink")
-
-def parkingspace(x):
-    ccount = x.count("C")
-    CCcount = x.count("CC") 
-    CCcount = CCcount * 2
-    print(ccount-CCcount)
-
-parkingspace("CC..C.CC.C.CC.C.") 
-
-def spaces(n,y,t):
-    occupied = 0
-    for i in range(n):
-        if y[i] == "C" and t[i] == "C":
-            occupied += 1 
-    return(occupied)
-
-print(spaces(5, "CCC..", ".C..C")) """
-
-def honicount():
-    honi = 0
-    for i in range:
-        if i == "H":
-            honi += 1 
-            if i == "O":
-                honi += 1
-                if i == "N":
-                    honi += 1
-                    if i == "I":
-                        honi += 1
-print(honicount)
