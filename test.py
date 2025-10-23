5b29b1d3011d6bd93429f17f50830ec408f74de9
mial-92 <mialin0170@gmail.com>
2025-10-23 10:17:26 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-MiasDealerShop = [
-    {"Item": "1. ella's poopy shoes", "Name": "ella's poopy shoes", "price": 0.56, "NameAndPrice": "ella's poopy shoes - 0.56", "description": "During a fire drill, third period, Ella step into some not so good, solid brown substance. It was unfortunate But thankfully she was able to wash it off! However, there may be some residue in the creases of her shoes."},                                                           
-    {"Item": "2. olivia's cat's hairballs", "Name": "olivia's cat's hairballs", "price": 4.99, "NameAndPrice": "olivia's cat's hairballs - 4.99", "description": "Olivia is allergic to cats, however, she still has two very adorable cats, Ollie and Ellie! Her cats are kinda hairy and she just has a bunch of hair stuck around her home."},
-    {"Item": "3. BigBackLiver's Gooner Fanfic(resale)", "Name": "BackLiver's Gooner Fanfic(resale)", "price": 350.00, "NameAndPrice": "BackLiver's Gooner Fanfic(resale) - 350.00", "description": "Collaboration with BigBack Liver's Library!! Buy now before it's too late..."},
-    {"Item": "4. Tina's BrainRot Guide", "Name": "Tina's BrainRot Guide", "price": 6.99, "NameAndPrice": "Tina's BrainRot Guide - 6.99", "description": "Tina has so much brainrot saved in her brain that it makes me feel way old even though im literally the same age as her."},
-    {"Item": "5. Xingwing's Twelve pc Soggy Chicken Wing", "Name": "Xingwing's Twelve pc Soggy Chicken Wing", "price": 12.99, "NameAndPrice": "Xingwing's Twelve pc Soggy Chicken Wing - 12.99", "description": "Xingwing is a nickname. She also sells wings but they're old and molding and smelly..."}, 
-    {"Item": "6. Maggie's decomposing bones", "Name": "Maggie's decomposing bones", "price": 34.99, "NameAndPrice": "Maggie's decomposing bones - 34.99", "description": "she's turning into dust"},
-    {"Item": "7. Mia's Fresh Cut Grass", "Name": "Mia's Fresh Cut Grass", "price": 68.99, "NameAndPrice": "Mia's Fresh Cut Grass - 68.99", "description": "Fresh Cut Grass from my backyard!!"}
+""" MiasDealerShop = [
+    {"Item": "1. ella's poopy shoes", "Name": "ella's poopy shoes", "price": 0.56, "description": "During a fire drill, third period, Ella step into some not so good, solid brown substance. It was unfortunate But thankfully she was able to wash it off! However, there may be some residue in the creases of her shoes."},                                                           
+    {"Item": "2. olivia's cat's hairballs", "Name": "olivia's cat's hairballs", "price": 4.99, "description": "Olivia is allergic to cats, however, she still has two very adorable cats, Ollie and Ellie! Her cats are kinda hairy and she just has a bunch of hair stuck around her home."},
+    {"Item": "3. BigBackLiver's Gooner Fanfic(resale)", "Name": "BackLiver's Gooner Fanfic(resale)", "price": 350.00, "description": "Collaboration with BigBack Liver's Library!! Buy now before it's too late..."},
+    {"Item": "4. Tina's BrainRot Guide", "Name": "Tina's BrainRot Guide", "price": 6.99, "description": "Tina has so much brainrot saved in her brain that it makes me feel way old even though im literally the same age as her."},
+    {"Item": "5. Xingwing's Twelve pc Soggy Chicken Wing", "Name": "Xingwing's Twelve pc Soggy Chicken Wing", "price": 12.99, "description": "Xingwing is a nickname. She also sells wings but they're old and molding and smelly..."}, 
+    {"Item": "6. Maggie's decomposing bones", "Name": "Maggie's decomposing bones", "price": 34.99, "description": "she's turning into dust"},
+    {"Item": "7. Mia's Fresh Cut Grass", "Name": "Mia's Fresh Cut Grass", "price": 68.99, "description": "Fresh Cut Grass from my backyard!!"}
 ]
 
 for i in range(0, len(MiasDealerShop)):
@@ -30,10 +30,48 @@ while consent == True:
                 consent = True
             if consent == "no":
                 print("Ok! Time to check out.") 
-    list.append(item["NameAndPrice"])
+    
 print(f"You are purchasing these items: {list} ")
 total = total * 1.08875
 rounded_total = round(total , 2)
 print(f"The total of your cart is going to be {rounded_total} with tax include")
 print("Thanks for shoppng with MiasDealerShop. Have a great day!")
 
+ """
+
+
+def casino(q , f , s , t):
+    plays = 0
+    f = 0
+    s = 0
+    t = 0
+    while q > 0:
+        f += 1
+        plays += 1
+        q = q - 1
+        if f == 35:
+            q = q + 30
+        elif s == 100:
+            q = q + 60 
+            t += 1
+            plays += 1
+            q = q - 1
+        elif t == 10:
+            q = q = 9
+        
+         
+    print(f"Martha can play her gambling machines {plays} times before going broke.")
+
+casino(48, 3, 10, 4)
+
+"""    s += 1 
+            plays += 1
+            q = q - 1
+        if s == 100:
+                q = q + 60
+        else: 
+            t += 1
+            plays += 1
+            q = q - 1
+        if t == 10:
+            q = q + 9 """
